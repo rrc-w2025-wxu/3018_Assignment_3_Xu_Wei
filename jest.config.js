00a681b016f232d8719ddc71910620c1bba7ee13
XU WEI <wxu48@academic.rrc.ca>
2026-03-01 12:50:30 -0600

    add: document of jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
+
+/**
+ * Jest configuration file
+ *
+ * This configuration sets up Jest to work with a TypeScript project
+ * using ts-jest and a Node.js test environment.
+ */
 module.exports = {
+    /**
+     * Use ts-jest preset to allow Jest to transpile TypeScript files
+     * before running tests.
+     */
     preset: "ts-jest",
+
+    /**
+     * Set the test environment to Node.js.
+     * This is required for backend/server-side testing.
+     */
     testEnvironment: "node",
+
+    /**
+     * Specify setup files to be executed after the test framework
+     * is installed in the environment.
+     * Typically used for global mocks or test initialization.
+     */
     setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
 };
